648347ea5bbd5679c2abf51500f60a93199fb768
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-05-04 11:15:49 +0300

    Update Sasha and His Trip.py

--- Sasha and His Trip.py
+++ Sasha and His Trip.py
@@ -1,17 +1,16 @@
 def main():
   n, v = map(int, input().split())
   run, res = 0, 0
-  if v >= n - 1:
+  if c >= n - 1:
       print(n - 1)
   else:
-      for i in range(1, n + 1):
-          want = n - i
-          if run < want:
-              res += (v - run) * i
-              run += v - run
-              run -= 1
-          else:
-              print(res)
-              break
+      ne = (n-1) - c
+      res += c
+      idx = 2
+      for i in range(ne):
+          res += idx
+          idx += 1
+      
+      print(res)
 
 main()
